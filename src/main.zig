3d3420f96f9186c95516c9e27291329ed36fb863
Sandalmoth <sandalmoth@pm.me>
2025-10-20 21:21:14 +0200

    change up the data types for the free lists

--- src/main.zig
+++ src/main.zig
@@ -8,8 +8,22 @@ pub fn main() !void {
     var hive: Hive(u32) = .empty;
     defer hive.deinit(gpa);
 
+    var refs: std.ArrayList(Hive(u32).Reference) = .empty;
+    defer refs.deinit(gpa);
+
+    var rng = std.Random.DefaultPrng.init(@bitCast(std.time.microTimestamp()));
+    const rand = rng.random();
+
     for (0..100) |i| {
         const ref = try hive.insert(gpa, @intCast(i));
         std.debug.print("{}\n", .{ref});
+        try refs.append(gpa, ref);
+    }
+
+    rand.shuffle(Hive(u32).Reference, refs.items);
+    for (refs.items) |ref| {
+        if (rand.boolean()) continue;
+        std.debug.print("{}\n", .{ref});
+        hive.erase(gpa, ref);
     }
 }
--- src/root.zig
+++ src/root.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
 
 pub fn Hive(comptime T: type) type {
-    const nil = std.math.maxInt(u48);
+    const nil = std.math.maxInt(usize);
     const min_capacity = @max(1, 64 / @sizeOf(T));
 
     return struct {
@@ -33,7 +33,8 @@ pub fn Hive(comptime T: type) type {
         const Segment = struct {
             const Header = struct {
                 capacity: usize,
-                next: Location,
+                next_segment: usize,
+                next_free_block: usize,
             };
 
             const Node = struct {
@@ -74,10 +75,8 @@ pub fn Hive(comptime T: type) type {
                 return .{
                     .head = .{
                         .capacity = capacity,
-                        .next = .{
-                            .segment = undefined,
-                            .offset = 0,
-                        },
+                        .next_free_block = 0,
+                        .next_segment = undefined,
                     },
                     .skip = skip,
                     .data = data,
@@ -100,14 +99,22 @@ pub fn Hive(comptime T: type) type {
         // kinda wish we didn't need both capacity and len but i don't see a way around it
         total_capacity: usize,
         len: usize,
-        next: u48,
+        next_segment: usize,
         segments: std.MultiArrayList(Segment),
         reserve: ?*Segment,
+
+        // NOTE on the construction of the free lists
+        // there is a list of segments starting with next_segment in the hive structure
+        // then in each segment there is a list of free blocks
+        // the next_segment and next_free_block are indices into segments and data respectively
+        // where std.math.maxInt(usize) (== nil) is used to indicate that there is no element
+        // the free blocks in each segment form a doubly linked list
+        // where loopback indices (e.g. a node at i pointing to i) is used to indicate no element
 
         pub const empty: Self = .{
             .total_capacity = 0,
             .len = 0,
-            .next = nil,
+            .next_segment = nil,
             .segments = .empty,
             .reserve = null,
         };
@@ -118,12 +125,13 @@ pub fn Hive(comptime T: type) type {
         }
 
         pub fn insert(hive: *Self, gpa: std.mem.Allocator, value: T) !Reference {
-            if (hive.next == nil) try hive.ensureUnusedCapacity(gpa, 1);
+            if (hive.next_segment == nil) try hive.ensureUnusedCapacity(gpa, 1);
             // fetch segment data
-            const head = &hive.segments.items(.head)[hive.next];
-            const skip = hive.segments.items(.skip)[hive.next];
-            const data = hive.segments.items(.data)[hive.next];
-            const ix = head.next.offset;
+            const ix_segment = hive.next_segment;
+            const head = &hive.segments.items(.head)[ix_segment];
+            const skip = hive.segments.items(.skip)[ix_segment];
+            const data = hive.segments.items(.data)[ix_segment];
+            const ix = head.next_free_block;
             std.debug.assert(skip[ix] > 0);
             std.debug.assert(skip[ix] == skip[ix + skip[ix] - 1]);
             const free_block = data[ix].node;
@@ -136,24 +144,28 @@ pub fn Hive(comptime T: type) type {
             // update erasure list
             if (free_block_len > 1) {
                 data[ix + 1] = .{ .node = .{
-                    .prev = ix + 1,
-                    .next = if (free_block.next != ix) free_block.next else ix + 1,
+                    .prev = @intCast(ix + 1),
+                    .next = if (free_block.next != ix) free_block.next else @intCast(ix + 1),
                 } };
-                head.next.offset += 1;
+                head.next_free_block += 1;
             } else {
                 // free block is exhausted
                 std.debug.assert(data[ix].node.prev == ix);
-                if (free_block.next != ix) data[free_block.next].node.prev = free_block.next;
-                head.next.offset = data[ix].node.next;
+                if (free_block.next != ix) {
+                    data[free_block.next].node.prev = free_block.next;
+                    head.next_free_block = data[ix].node.next;
+                } else {
+                    // segment is completely full
+                    hive.next_segment = head.next_segment;
+                    head.next_segment = nil;
+                }
             }
             data[ix] = .{ .value = value };
             hive.len += 1;
 
-            // TODO if the segment is full, remove it from the segment free list
-
             return .fromLocation(.{
-                .segment = hive.next,
-                .offset = ix,
+                .segment = @intCast(ix_segment),
+                .offset = @intCast(ix),
             });
         }
 
@@ -171,11 +183,55 @@ pub fn Hive(comptime T: type) type {
                 try hive.segments.ensureUnusedCapacity(gpa, 1);
                 var segment: Segment = try .create(gpa, @intCast(capacity));
                 // prepend to list of segments with free slots
-                segment.head.next.segment = hive.next;
-                hive.next = @intCast(hive.segments.len);
+                segment.head.next_segment = hive.next_segment;
+                hive.next_segment = @intCast(hive.segments.len);
                 hive.segments.appendAssumeCapacity(segment);
                 hive.total_capacity += capacity;
             }
+        }
+
+        pub fn erase(hive: *Self, gpa: std.mem.Allocator, ref: Reference) void {
+            _ = gpa;
+            const loc: Location = .fromReference(ref);
+            const ix_segment: usize = @intCast(loc.segment);
+            const ix: usize = loc.offset;
+
+            const head = &hive.segments.items(.head)[ix_segment];
+            const skip = hive.segments.items(.skip)[ix_segment];
+            const data = hive.segments.items(.data)[ix_segment];
+
+            const value = data[ix];
+            hive.len -= 1;
+
+            // there are four options for the free block
+            // a) both neighbours occupied, form new free block
+            // b/c) one neighbour occupied (left/right), extend free block
+            // d) both neighbours free, merge into the free block on the left
+            // and the way to determine the case is to look at the skipfields
+            const skip_left = if (ix == 0) 0 else skip[ix - 1];
+            const skip_right = skip[ix + 1]; // NOTE may index into the padding skipfield
+            if (skip_left == 0 and skip_right == 0) {
+                skip[ix] = 1;
+                data[ix] = .{ .node = .{
+                    .prev = @intCast(ix),
+                    .next = if (head.next_free_block == nil)
+                        @intCast(ix)
+                    else
+                        head.next_free_block,
+                } };
+                head.next_free_block = @intCast(ix);
+            } else if (skip_left > 0 and skip_right == 0) {
+                skip[ix - skip[ix - 1]] = skip_left + 1;
+                skip[ix] = skip_left + 1;
+            } else if (skip_left == 0 and skip_right < 0) {
+                //
+            } else if (skip_left > 0 and skip_right > 0) {
+                //
+            } else unreachable;
+
+            // TODO test if the segment is empty
+
+            return value;
         }
     };
 }
